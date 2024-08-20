1d5cb68648f3755a86bf4dd366e2077d11f502e9
unknown <bodaahmed2500@gmail.com>
2024-08-20 18:11:36 +0300

    add change color and font

--- main.js
+++ main.js
@@ -7,6 +7,10 @@ let open=document.getElementById("open");
 let hero=document.getElementById("hero");
 let footer=document.getElementById("footer");
 let image=document.getElementById("image");
+let ch1=document.getElementById("ch1");
+let f1=document.getElementById("f1");
+let f2=document.getElementById("f2");
+let f3=document.getElementById("f3");
 bars.addEventListener("click",()=>{
     sidebars.style.display="block";
 })
@@ -27,3 +31,17 @@ open.addEventListener("click",()=>{
 if(colors.style.display=="none") {
     open.style.right="0";
 }
+ch1.addEventListener("click",()=>{
+    hero.style.backgroundColor="#f3f1f6"
+    footer.style.backgroundColor="#f3f1f6"
+});
+f1.addEventListener("click",()=>{
+    document.body.style.fontFamily="Helvetica";
+});
+f2.addEventListener("click",()=>{
+    document.body.style.fontFamily="Edu VIC WA NT Beginner";
+});
+f3.addEventListener("click",()=>{
+    console.log("Click")
+    document.body.style.fontFamily="Bungee Tint";
+});
